6000762c8d50c823197a6804e044929eecbda0cd
Dan Wahlin <dwahlin@xmlforasp.net>
2022-02-12 00:58:46 -0700

    first run of updating learn content in services

--- learnParser.js
+++ learnParser.js
@@ -1,39 +1,34 @@
 'use strict';
 
 const fs = require('fs'),
-    request = require('request'),
+    axios = require('axios'),
     cheerio = require('cheerio'),
-    download = require('image-downloader'),
-    userArgs = process.argv.slice(2);
+    download = require('image-downloader')
 
 loadServices();
 
-function loadServices() {
+async function loadServices() {
     const services = JSON.parse(fs.readFileSync(__dirname + '/public/data/services.json', 'utf8'));
 
     for (let service of services) {
         if (service.learnContent) {
             for (let learnContent of service.learnContent) {
                 if (learnContent.type === 'path' && !learnContent.modules) {
-                    getHtml(learnContent.url, service, learnContent); 
+                    const {title, image, modules} = await getHtml(learnContent.url, service, learnContent); 
+                    learnContent.title = title;
+                    learnContent.image = image;
+                    learnContent.modules = modules;
                 }
             }
         }
     }
+
+    fs.writeFileSync(__dirname + '/public/data/services.json', JSON.stringify(services, '', 2));
 }
 
-function getHtml(url, service, learnContent) {   
-    var options = {
-        url: url,
-        header: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36',
-            'Accept': '*/*'
-        }
-    };
-    
-    request(options, function (err, resp, body) {
-        parseData(body, service, learnContent);
-    });
+async function getHtml(url, service, learnContent) {   
+    const response = await axios.get(url);
+    return parseData(response.data, service, learnContent);
 }
 
 function parseData(html, service, learnContent) {
@@ -42,7 +37,7 @@ function parseData(html, service, learnContent) {
 
     const modules = [];
     // get header info
-    const title = $('title').text();
+    const title = $('h1.title').text();
     const image = prefix + $('img[role="presentation"]').attr('src').replace('/en-us/', '');
 
     $('div[data-bi-name=module] div.column>a').map((index, element) => {
@@ -56,8 +51,8 @@ function parseData(html, service, learnContent) {
         }
     });
 
-    learnContent.modules = modules;
-    console.log(service.name, learnContent.modules);
+    console.log(service.name, modules);
+    return {title, image, modules};
 }
 
 function downloadImage(url, filepath) {
