ed669ebc0cedcb9121ac70448bc4f092ea4d9685
Dan Wahlin <dwahlin@xmlforasp.net>
2022-02-12 00:42:21 -0700

    Initial work on learnParser

---
+++ learnParser.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs'),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    download = require('image-downloader'),
+    userArgs = process.argv.slice(2);
+
+loadServices();
+
+function loadServices() {
+    const services = JSON.parse(fs.readFileSync(__dirname + '/public/data/services.json', 'utf8'));
+
+    for (let service of services) {
+        if (service.learnContent) {
+            for (let learnContent of service.learnContent) {
+                if (learnContent.type === 'path' && !learnContent.modules) {
+                    getHtml(learnContent.url, service, learnContent); 
+                }
+            }
+        }
+    }
+}
+
+function getHtml(url, service, learnContent) {   
+    var options = {
+        url: url,
+        header: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36',
+            'Accept': '*/*'
+        }
+    };
+    
+    request(options, function (err, resp, body) {
+        parseData(body, service, learnContent);
+    });
+}
+
+function parseData(html, service, learnContent) {
+    const $ = cheerio.load(html);
+    const prefix = 'https://docs.microsoft.com/';
+
+    const modules = [];
+    // get header info
+    const title = $('title').text();
+    const image = prefix + $('img[role="presentation"]').attr('src').replace('/en-us/', '');
+
+    $('div[data-bi-name=module] div.column>a').map((index, element) => {
+        const url = element.attribs.href.trim().replace('../../', 'https://docs.microsoft.com/learn/');
+        const moduleName = $(element).find('h3').text().trim();
+        if (url) {
+            modules.push({ 
+                name: moduleName,
+                url
+            });
+        }
+    });
+
+    learnContent.modules = modules;
+    console.log(service.name, learnContent.modules);
+}
+
+function downloadImage(url, filepath) {
+    return download.image({
+        url,
+        dest: filepath 
+    });
+}
+
+
+
